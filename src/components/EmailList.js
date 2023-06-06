aa4b96b62ddca0586c10dc253778ef03064b4149
deyangotsev <deyangotsev10@gmail.com>
2023-06-06 21:53:18 +0300

    EmailList and EmailRow component

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import '../styles/EmailList.css';    
-import { ArrowDropDown, CheckBoxOutlineBlank, MoreVert, Redo } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
+import '../styles/EmailList.css';
+import { ArrowDropDown,  ChevronLeft, ChevronRight, Inbox, KeyboardHide, LocalOffer, MoreVert, People, Redo, Settings } from '@mui/icons-material';
+import { Checkbox, IconButton } from '@mui/material';
+import Section from './Section';
+import EmailRow from './EmailRow';
 
 
 function EmailList() {
@@ -9,7 +11,7 @@ function EmailList() {
     <div className='emailList'>
       <div className="emailList_settings">
         <div className="emailList_settingsLeft">
-          <CheckBoxOutlineBlank />
+          <Checkbox />
 
           <IconButton>
             <ArrowDropDown />
@@ -23,6 +25,36 @@ function EmailList() {
             <MoreVert />
           </IconButton>
         </div>
+
+        <div className="emailList_settingsRight">
+          <IconButton>
+            <ChevronLeft />
+          </IconButton>
+
+          <IconButton>
+            <ChevronRight />
+          </IconButton>
+
+          <IconButton>
+            <KeyboardHide />
+          </IconButton>
+
+          <IconButton>
+            <Settings />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="emailList_sections">
+        <Section Icon={Inbox} title='Primary' color='red' selected />
+        <Section Icon={People} title='Social' color='#1A73E8' />
+        <Section Icon={LocalOffer} title='Promotions' color='green' />
+      </div>
+
+      <div className="emailList_list">
+        <EmailRow title='Deyan' subject='About the Rent' description='When are you going to pay ?' time='10pm'/>
+        <EmailRow title='Elon Musk' subject='Need more memes' description='Hey, I need a funny meme for Twitter! Can you share me something funny.' />
+        
       </div>
     </div>
   )
